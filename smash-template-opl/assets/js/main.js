85652dd4315fb7d5a168a25ab3ab6a807ac5c9f2
Remington Anton Amalanathan <remigntonanton@gmail.com>
2021-11-28 12:15:23 +0000

    add image upload and cheat's gallery

--- smash-template-opl/assets/js/main.js
+++ smash-template-opl/assets/js/main.js
@@ -120,7 +120,7 @@ $(function() {
         });
 
         // filter items on button click
-        $('.portfolio-menu ul').on('click', 'li', function () {
+        $('.portfolio-menu ul').on('click', 'li', showFileName,  function () {
             var filterValue = $(this).attr('data-filter');
             $grid.isotope({
                 filter: filterValue
@@ -195,6 +195,44 @@ $(function() {
     
     
     
+/*  ==========================================
+    SHOW UPLOADED IMAGE
+* ========================================== */
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+
+/*  ==========================================
+    SHOW UPLOADED IMAGE NAME
+
+    Here input is the image 
+* ========================================== */
+var input = document.getElementById( 'upload' );
+var infoArea = document.getElementById( 'upload-label' );
+
+input.addEventListener( 'change', showFileName );
+
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
+
+
     
     
     
